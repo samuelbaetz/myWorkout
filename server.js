950b4455ca9edfbe2cd9ea1cc1ca3fe4dfc9e417
Samuel Baetz <samuelbaetz105@gmail.com>
2021-01-17 19:13:39 -0500

    deploy

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/myworkout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/myworkout", {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true,useFindAndModify: false});
+
+
 
 app.get("/api/workouts", (req, res) => {
     Workout.Workout.find({})
